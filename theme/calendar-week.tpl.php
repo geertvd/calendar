72dd88afc453a1fe716e72db610d38beda6c6eac
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-05-19 23:38:34 +0000

    More work on creating template files for all the calendar parts. Only the month view is working at all, and all of them need more work.

---
+++ theme/calendar-week.tpl.php
@@ -0,0 +1,47 @@
+<?php
+// $Id$
+/**
+ * @file
+ * Template to display a view as a calendar month.
+ * 
+ * @see template_preprocess_calendar_month.
+ *
+ * - $view: The view.
+ * - $links: Array of formatted links to other calendar displays, i.e. day, year.
+ * - $day_names: An array of the day of week names for the table header.
+ * - $rows: An array of data for each day of the week.
+ * - $calendar_nav: Formatted back/next navigation links.
+ *     @see calendar-nav.tpl.php.
+ * 
+ */
+dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. $max_date_formatted);
+?>
+
+<div class="calendar-calendar"><div class="week-view">
+
+<?php if (empty($mini)) print theme('links', $calendar_links);?>
+
+<table <?php if ($mini): ?> class="mini"<? endif; ?>>
+  <thead>
+    <?php print $calendar_nav ?>
+    <tr>
+      <?php foreach ($day_names as $cell): ?>
+        <th id="<?php print $cell['id']; ?>" class="views-field <?php print $cell['class']; ?>">
+          <?php print $cell['data']; ?>
+        </th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach ($rows as $row): ?>
+      <tr>
+        <?php foreach ($row as $cell): ?>
+          <td id="<?php print $cell['id']; ?>" class="views-field <?php print $cell['class']; ?>">
+            <?php print $cell['data']; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+</div></div>
---
+++ theme/calendar-year.tpl.php
@@ -0,0 +1,36 @@
+<?php
+// $Id$
+/**
+ * @file
+ * Template to display a view as a calendar month.
+ * 
+ * @see template_preprocess_calendar_month.
+ *
+ * - $view: The view.
+ * - $links: Array of formatted links to other calendar displays, i.e. day, year.
+ * - $day_names: An array of the day of week names for the table header.
+ * - $rows: An array of data for each day of the week.
+ * - $calendar_nav: Formatted back/next navigation links.
+ *     @see calendar-nav.tpl.php.
+ * 
+ */
+dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. $max_date_formatted);
+
+?>
+
+<div class="calendar-calendar"><div class="year-view">
+
+<?php if (empty($mini)) print theme('links', $calendar_links);?>
+
+<table <?php if ($mini): ?> class="mini"<? endif; ?>>
+  <thead>
+    <?php print $calendar_nav ?>
+  </thead>
+  <tbody>
+    <tr><td><?php print $months[1] ?></td><td><?php print $months[2] ?></td><td><?php print $months[3] ?></td></tr>  
+    <tr><td><?php print $months[4] ?></td><td><?php print $months[5] ?></td><td><?php print $months[6] ?></td></tr>  
+    <tr><td><?php print $months[7] ?></td><td><?php print $months[8] ?></td><td><?php print $months[9] ?></td></tr>  
+    <tr><td><?php print $months[10] ?></td><td><?php print $months[11] ?></td><td><?php print $months[12] ?></td></tr>  
+  </tbody>
+</table>
+</div></div>
