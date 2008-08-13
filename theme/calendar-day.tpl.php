e3e44ee1ef38b61c9d2f8f74ac4a627b5ea16123
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-08-13 11:43:50 +0000

    Add some explanation to the day template about why a table is used even though it is a single block of text.

--- theme/calendar-day.tpl.php
+++ theme/calendar-day.tpl.php
@@ -7,10 +7,13 @@
  * @see template_preprocess_calendar_day.
  *
  * $day_names: The day of week info for the table header.
- * $rows: An array of data for this day.
+ * $rows: The rendered data for this day.
  * $view: The view.
  * $min_date_formatted: The minimum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
  * $max_date_formatted: The maximum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
+ * 
+ * We use a table here just for consistency with the other views so the
+ * styles and css will work the same for the day views as for the others.
  */
 //dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. $max_date_formatted);
 ?>
