2dd4fd3a8d6636a0eb6b58358af6f2f061c41e4e
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-11-18 14:05:07 +0000

    Adapt processing to make sure only selected dates display in nodes, views, and calendars.

--- theme/calendar-day.tpl.php
+++ theme/calendar-day.tpl.php
@@ -2,7 +2,8 @@
 // $Id$
 /**
  * @file
- * Template to display a view as a calendar day.
+ * Template to display a view as a calendar day, grouped by time
+ * and optionally organized into columns by a field value.
  * 
  * @see template_preprocess_calendar_day.
  *
@@ -12,39 +13,70 @@
  * $rows['empty'] - empty text for this day, if no items were found.
  * $rows['all_day'] - an array of formatted all day items.
  * $rows['items'] - an array of timed items for the day.
- * $rows['items']['hour'] - the formatted hour.
- * $rows['items']['ampm'] - the formatted ampm value, if any.
- * $rows['items']['values'] - an array of formatted items for this hour.
+ * $rows['items'][$time_period]['hour'] - the formatted hour for a time period.
+ * $rows['items'][$time_period]['ampm'] - the formatted ampm value, if any for a time period.
+ * $rows['items'][$time_period][$column]['values'] - An array of formatted 
+ *   items for a time period and field column.
  * 
  * $view: The view.
+ * $columns: an array of column names.
  * $min_date_formatted: The minimum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
  * $max_date_formatted: The maximum date for this calendar in the format YYYY-MM-DD HH:MM:SS.
  * 
+ * The width of the columns is dynamically set using <col></col> 
+ * based on the number of columns presented. The values passed in will
+ * work to set the 'hour' column to 10% and split the remaining columns 
+ * evenly over the remaining 90% of the table.
  */
 //dsm('Display: '. $display_type .': '. $min_date_formatted .' to '. $max_date_formatted);
+//dsm($columns);
 //dsm($rows);
 ?>
 
 <div class="calendar-calendar"><div class="day-view">
 <table>
+  <thead>
+    <col width="10%"></col>
+    <?php foreach ($columns as $column): ?>
+    <col width="<?php print $column_width; ?>%"></col>
+    <?php endforeach; ?>
+    <tr>
+      <th class="calendar-dayview-hour"><?php t('Time'); ?></th>
+      <?php foreach ($columns as $column): ?>
+      <th class="calendar-agenda-items"><?php print $column; ?></th>
+      <?php endforeach; ?>
+    </tr>
+  </thead>
   <tbody>
     <tr>
-      <td class="calendar-dayview-hour">
+      <td class="calendar-agenda-hour">
          <span class="calendar-hour"><?php print t('All day'); ?></span>
        </td>
-       <td class="calendar-dayview-items">
-         <div class="calendar"><div class="inner"><?php print implode($rows['all_day']); ?></div></div>
+      <?php foreach ($columns as $column): ?>
+       <td class="calendar-agenda-items">
+         <div class="calendar">
+         <div class="inner">
+           <?php print isset($rows['all_day'][$column]) ? implode($rows['all_day'][$column]) : '&nbsp;';?>
+         </div>
+         </div>
        </td>
+      <?php endforeach; ?>   
     </tr>
     <?php foreach ($rows['items'] as $hour): ?>
     <tr>
-      <td class="calendar-dayview-hour">
+      <td class="calendar-agenda-hour">
         <span class="calendar-hour"><?php print $hour['hour']; ?></span>
         <span class="calendar-ampm"><?php print $hour['ampm']; ?></span>
       </td>
-      <td class="calendar-dayview-items">
-        <div class="calendar"><div class="inner"><?php print implode($hour['values']); ?></div></div>
-      </td>
+      <?php foreach ($columns as $column): ?>
+        <td class="calendar-agenda-items">
+          <div class="calendar">
+          <div class="inner">
+            <?php print isset($hour['values'][$column]) ? implode($hour['values'][$column]) : '&nbsp;'; ?>
+          </div>
+          </div>
+        </td>
+      <?php endforeach; ?>   
     </tr>
    <?php endforeach; ?>   
   </tbody>
