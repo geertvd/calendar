c2c3443896e50f457fd07dc4b0972d5a29c8a817
Karen Stevenson <karens@45874.no-reply.drupal.org>
2008-11-18 15:02:34 +0000

    Revert changes accidentally included in last commit which should have only included changes to control the right date display.

--- theme/calendar-month-node.tpl.php
+++ theme/calendar-month-node.tpl.php
@@ -36,7 +36,7 @@
  *   print date_format($calendar_start_date, 'l, j F Y - g:ia');
  * 
  * @see template_preprocess_calendar_month_node.
- */
+ */ 
 ?>
 <div class="view-item view-item-<?php print $view->name ?>">
   <div class="calendar monthview" id="<?php print $node->date_id ?>">
