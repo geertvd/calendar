e62762ea9e815ee5dbaba453713a6f5a22073377
Karen Stevenson <karen@elderweb.com>
2011-08-18 07:23:39 -0500

    Add option to change method from PUBLISH to REQUEST in VCALENDAR.

--- calendar_ical/calendar-style-ical.tpl.php
+++ calendar_ical/calendar-style-ical.tpl.php
@@ -3,11 +3,13 @@
  * $title
  *   The name of the calendar.
  */
-
+if (empty($method)) {
+  $method = 'PUBLISH';
+}
 ?>
 BEGIN:VCALENDAR
 VERSION:2.0
-METHOD:PUBLISH
+METHOD:<?php print $method; ?>
 X-WR-CALNAME;VALUE=TEXT:<?php print $title . "\r\n"; ?>
 PRODID:-//Drupal iCal API//EN
 <?php print $rows ?>
