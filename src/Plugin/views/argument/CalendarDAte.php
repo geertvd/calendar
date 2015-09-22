57a71a18f3e9dde33e3e55fe89be15310c0007a1
Pieter Jonckiere <pieter.jonckiere@xio.be>
2015-09-22 11:29:54 +0200

    Added calendar argument handler and removed legacy views.inc file

---
+++ src/Plugin/views/argument/CalendarDAte.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\calendar\Plugin\views\argument\CalendarDate.
+ */
+
+namespace Drupal\calendar\Plugin\views\argument;
+
+use Drupal\views\Plugin\views\argument\Date;
+
+/**
+ * Calendar argument handler for a date.
+ *
+ * @ingroup views_argument_handlers
+ *
+ * @ViewsArgument("calendar_datetime")
+ */
+class CalendarDate extends Date {
+
+}
